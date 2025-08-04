fdf52f6f900b869805b008b14c8c1506bcea6159
Zeeshanx01 <zeeshanmunir13579@gmail.com>
2025-08-04 14:33:01 +0500

    2025-08-04_14:32

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="w-full text-sm text-center fade-in bg-black bg-opacity-40 backdrop-blur-sm h-[5vh] sticky bottom-0 z-20 flex justify-center items-center">
       <a className="text-gray-400 no-underline hover:no-underline" href="#">&copy; Online Food Delivery System</a>
       <span className='mx-1'>- Developed by</span>
-      <a className="text-gray-400 no-underline hover:no-underline" href="https://www.tailwindtoolbox.com">Zeeshan, Abbas and Saif</a>
+      <a className="text-gray-400 no-underline hover:no-underline" href="https://www.tailwindtoolbox.com">Zeeshan Munir</a>
     </div>
   )
 }
